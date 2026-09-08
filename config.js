1d3fa3cc60320280c431e397297f84a4474a41db
UrbanAnimeTv <UrbanAnimeTv10@gmail.com>
2026-09-08 06:15:01 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.URBANANIMETV_CONFIG = {
   SUPABASE_URL: "https://rxmedycaaudgxygnmovh.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "PASTE_YOUR_PUBLISHABLE_KEY_HERE"
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_-LcVvbkEdwfawcFOzyX_jA_rb9M1Of8"
 };
